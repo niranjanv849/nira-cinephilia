0fbe31c0f7284df5b41551964a811a6876cf8f99
Niranjan849 <niranjan.ct19@bitsathy.ac.in>
2021-12-30 15:02:45 +0530

    Finished except dep

---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import logo from "./logo.png";
+import avatar from "./avatar.jpg";
+import "./Nav.css";
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img className="nav__logo" src={logo} alt="NIRA LOGO" />
+      <img className="nav__avatar" src={avatar} alt="AVATAR" />
+    </div>
+  );
+}
+    
+export default Nav;
